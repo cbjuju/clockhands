85b2d31041c7317283074eb3dbb8ccfa855d2c02
Sudip Kunda <sudipkunda@gmail.com>
2022-03-12 11:42:58 -0600

    Screen initialized

--- hands.py
+++ hands.py
@@ -1,11 +1,20 @@
 import pygame
+import time
 
 current_time = 0
 total_time   = 15
 time_step    = 0.1
 
+screen = pygame.display.set_mode((400, 400))
+
 while current_time < total_time:
 
+    for event in pygame.event.get():
+
+        if event.type == pygame.QUIT:
+            exit()
+
     current_time += time_step
+    time.sleep(0.1)
 
 print ("finished")
